8e4e584be724c9b3703d3375e98fc62c0aff15bd
Víctor <mena121997@gmail.com>
2023-06-19 11:18:35 +0200

    miembros + selector

---
+++ AppCoc/selector.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aplicaciones</title>
+  <link rel="stylesheet" type="text/css" href="css/selector.css">
+</head>
+<body>
+    <header class="header">
+        <div class="contendor">
+            <h1 class="titulo">Apps Victor</h1>
+        </div>
+    </header>
+     
+    <section>
+      <a href="archivos/miembros.php">
+        Gestion de Sanciones CoC
+        <img src="images/coc.png" alt="Mantenimiento1">
+        <img src="images/coc2.png" alt="Mantenimiento2">
+      </a>
+    </section>
+</body>
+</html>
